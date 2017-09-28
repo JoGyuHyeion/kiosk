363a47cb30e2be9f255c88e61866379f168ceb6a
kimjjye <rkawk4975@gmail.com>
2017-09-28 17:44:21 +0900

    merge 후 오류 체크 확인함 from jo

--- ki_project/src/main/java/org/zerock/persistence/Com_staffDAO.java
+++ ki_project/src/main/java/org/zerock/persistence/Com_staffDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.zerock.persistence;
 
 import java.util.List;
@@ -32,38 +31,4 @@ public interface Com_staffDAO {
   public int listSearchCount(SearchCriteria cri)throws Exception;
 
 }
-=======
-package org.zerock.persistence;
 
-import java.util.List;
-
-import org.zerock.domain.Com_staffVO;
-import org.zerock.domain.Criteria;
-import org.zerock.domain.SearchCriteria;
-
-public interface Com_staffDAO {
-
-  public void create(Com_staffVO vo) throws Exception;
-
-  public Com_staffVO read(Integer st_no) throws Exception;
-
-  public void update(Com_staffVO vo) throws Exception;
-
-  public void delete(Integer st_no) throws Exception;
-
-  public List<Com_staffVO> listAll() throws Exception;
-
-  public List<Com_staffVO> listPage(int page) throws Exception;
-
-  public List<Com_staffVO> listCriteria(Criteria cri) throws Exception;
-
-  public int countPaging(Criteria cri) throws Exception;
-  
-  //use for dynamic sql
-  
-  public List<Com_staffVO> listSearch(SearchCriteria cri)throws Exception;
-  
-  public int listSearchCount(SearchCriteria cri)throws Exception;
-
-}
->>>>>>> ab9babf5047782ebfc1d6dbed110703b09390fc2
