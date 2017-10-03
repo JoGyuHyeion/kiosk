d73232105b08742bb56ae2a8b1b344f6af315860
JoGyuHyeion <kuhyun1993@naver.com>
2017-10-03 17:00:44 +0900

    revert_jo

---
+++ ki_project/src/main/java/org/zerock/service/TeamsService.java
@@ -0,0 +1,18 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.zerock.dto.TeamsDTO;
+
+public interface TeamsService {
+	
+//	  public void create(TeamsDTO dto) throws Exception;
+
+	  public TeamsDTO read(Integer no) throws Exception;
+
+//	  public void update(TeamsDTO dto) throws Exception;
+
+//	  public void delete(Integer no) throws Exception;
+	  
+//	  public List<TeamsDTO> listAll() throws Exception;
+}
