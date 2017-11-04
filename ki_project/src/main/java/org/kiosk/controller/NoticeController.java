dc87e2e25130e3d80edf145358bbd1fcb6e425b2
JoGyuHyeion <kuhyun1993@naver.com>
2017-11-04 21:05:31 +0900

    rename controller

--- ki_project/src/main/java/org/kiosk/controller/NoticeController.java
+++ ki_project/src/main/java/org/kiosk/controller/NoticeController.java
@@ -1,12 +1,11 @@
 package org.kiosk.controller;
 
-import javax.annotation.Resource;
 import javax.inject.Inject;
-import org.kiosk.domain.Com_staffVO;
+
+import org.kiosk.domain.Com_boardVO;
 import org.kiosk.domain.PageMaker;
 import org.kiosk.domain.SearchCriteria;
-import org.kiosk.service.Com_staffService;
-import org.kiosk.util.UploadFileUtils;
+import org.kiosk.service.Com_boardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -15,27 +14,19 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/sboard/*")
-public class SearchBoardController {
-
-	private static final Logger logger = LoggerFactory.getLogger(SearchBoardController.class);
+@RequestMapping("/noticeboard/*")
+public class NoticeController {
+	private static final Logger logger = LoggerFactory.getLogger(NoticeController.class);
 
 	@Inject
-	private Com_staffService service;
-
-	@Resource(name = "uploadPath")
-	private String uploadPath;
-
-	private String img_fileName = "staff_";
-	private String[] dirPath = { "staff" };
-	//필요에 따라  arraylist로 원하는 항목을  add 하여 array 변환하면 유동적인 path를 생성할수있다.
+	private Com_boardService service;
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public void listPage(@ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
+	public void listPage(@ModelAttribute("cri") SearchCriteria cri, Model model)
+			throws Exception {
 
 		logger.info(cri.toString());
 
@@ -50,17 +41,18 @@ public class SearchBoardController {
 	}
 
 	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
-	public void read(@RequestParam("st_no") int st_no, @ModelAttribute("cri") SearchCriteria cri, Model model)
+	public void read(@RequestParam("bbs_no") int bbs_no, @ModelAttribute("cri") SearchCriteria cri, Model model)
 			throws Exception {
 
-		model.addAttribute(service.read(st_no));
+		model.addAttribute(service.read(bbs_no));
+		
 	}
 
 	@RequestMapping(value = "/removePage", method = RequestMethod.POST)
-	public String remove(@RequestParam("st_no") int st_no, SearchCriteria cri, RedirectAttributes rttr)
+	public String remove(@RequestParam("bbs_no") int bbs_no, SearchCriteria cri, RedirectAttributes rttr)
 			throws Exception {
 
-		service.remove(st_no);
+		service.remove(bbs_no);
 
 		rttr.addAttribute("page", cri.getPage());
 		rttr.addAttribute("perPageNum", cri.getPerPageNum());
@@ -69,19 +61,17 @@ public class SearchBoardController {
 
 		rttr.addFlashAttribute("msg", "SUCCESS");
 
-		return "redirect:/sboard/list";
+		return "redirect:/noticeboard/list?page=1";
 	}
 
 	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
-	public void modifyPagingGET(int st_no, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
+	public void modifyPagingGET(int bbs_no, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
 
-		model.addAttribute(service.read(st_no));
-		logger.info(service.read(st_no).toString());
+		model.addAttribute(service.read(bbs_no));
 	}
 
 	@RequestMapping(value = "/modifyPage", method = RequestMethod.POST)
-	public String modifyPagingPOST(Com_staffVO board, SearchCriteria cri, RedirectAttributes rttr) throws Exception {
-
+	public String modifyPagingPOST(Com_boardVO board, SearchCriteria cri, RedirectAttributes rttr) throws Exception {
 		logger.info(cri.toString());
 		service.modify(board);
 
@@ -94,30 +84,25 @@ public class SearchBoardController {
 
 		logger.info(rttr.toString());
 
-		return "redirect:/sboard/list";
+		return "redirect:/noticeboard/list";
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public void registGET(@ModelAttribute("cri") SearchCriteria cri) throws Exception {
+	public void registGET() throws Exception {
 
 		logger.info("regist get ...........");
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String registPOST(Com_staffVO board, RedirectAttributes rttr, @RequestParam("imgFile") MultipartFile imgFile)
-			throws Exception {
+	public String registPOST(Com_boardVO board, RedirectAttributes rttr) throws Exception {
 
 		logger.info("regist post ...........");
 		logger.info(board.toString());
 
-		String img_filenm = UploadFileUtils.uploadImageFile(uploadPath, imgFile.getOriginalFilename(),
-				imgFile.getBytes(), img_fileName + (service.maxNum() + 1), dirPath);
-		board.setImg_filenm(img_filenm);
 		service.regist(board);
 
 		rttr.addFlashAttribute("msg", "SUCCESS");
 
-		return "redirect:/sboard/list";
+		return "redirect:/noticeboard/list?page=1";
 	}
-
 }
--- ki_project/src/main/java/org/kiosk/controller/StaffBoardController.java
+++ ki_project/src/main/java/org/kiosk/controller/StaffBoardController.java
@@ -1,11 +1,12 @@
 package org.kiosk.controller;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
-
-import org.kiosk.domain.Com_boardVO;
+import org.kiosk.domain.Com_staffVO;
 import org.kiosk.domain.PageMaker;
 import org.kiosk.domain.SearchCriteria;
-import org.kiosk.service.Com_boardService;
+import org.kiosk.service.Com_staffService;
+import org.kiosk.util.UploadFileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -14,19 +15,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/noticeboard/*")
-public class SearchNoticeController {
-	private static final Logger logger = LoggerFactory.getLogger(SearchNoticeController.class);
+@RequestMapping("/sboard/*")
+public class StaffBoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(StaffBoardController.class);
 
 	@Inject
-	private Com_boardService service;
+	private Com_staffService service;
+
+	@Resource(name = "uploadPath")
+	private String uploadPath;
+
+	private String img_fileName = "staff_";
+	private String[] dirPath = { "staff" };
+	//필요에 따라  arraylist로 원하는 항목을  add 하여 array 변환하면 유동적인 path를 생성할수있다.
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public void listPage(@ModelAttribute("cri") SearchCriteria cri, Model model)
-			throws Exception {
+	public void listPage(@ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
 
 		logger.info(cri.toString());
 
@@ -41,18 +50,17 @@ public class SearchNoticeController {
 	}
 
 	@RequestMapping(value = "/readPage", method = RequestMethod.GET)
-	public void read(@RequestParam("bbs_no") int bbs_no, @ModelAttribute("cri") SearchCriteria cri, Model model)
+	public void read(@RequestParam("st_no") int st_no, @ModelAttribute("cri") SearchCriteria cri, Model model)
 			throws Exception {
 
-		model.addAttribute(service.read(bbs_no));
-		
+		model.addAttribute(service.read(st_no));
 	}
 
 	@RequestMapping(value = "/removePage", method = RequestMethod.POST)
-	public String remove(@RequestParam("bbs_no") int bbs_no, SearchCriteria cri, RedirectAttributes rttr)
+	public String remove(@RequestParam("st_no") int st_no, SearchCriteria cri, RedirectAttributes rttr)
 			throws Exception {
 
-		service.remove(bbs_no);
+		service.remove(st_no);
 
 		rttr.addAttribute("page", cri.getPage());
 		rttr.addAttribute("perPageNum", cri.getPerPageNum());
@@ -61,17 +69,19 @@ public class SearchNoticeController {
 
 		rttr.addFlashAttribute("msg", "SUCCESS");
 
-		return "redirect:/noticeboard/list?page=1";
+		return "redirect:/sboard/list";
 	}
 
 	@RequestMapping(value = "/modifyPage", method = RequestMethod.GET)
-	public void modifyPagingGET(int bbs_no, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
+	public void modifyPagingGET(int st_no, @ModelAttribute("cri") SearchCriteria cri, Model model) throws Exception {
 
-		model.addAttribute(service.read(bbs_no));
+		model.addAttribute(service.read(st_no));
+		logger.info(service.read(st_no).toString());
 	}
 
 	@RequestMapping(value = "/modifyPage", method = RequestMethod.POST)
-	public String modifyPagingPOST(Com_boardVO board, SearchCriteria cri, RedirectAttributes rttr) throws Exception {
+	public String modifyPagingPOST(Com_staffVO board, SearchCriteria cri, RedirectAttributes rttr) throws Exception {
+
 		logger.info(cri.toString());
 		service.modify(board);
 
@@ -84,25 +94,30 @@ public class SearchNoticeController {
 
 		logger.info(rttr.toString());
 
-		return "redirect:/noticeboard/list";
+		return "redirect:/sboard/list";
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
-	public void registGET() throws Exception {
+	public void registGET(@ModelAttribute("cri") SearchCriteria cri) throws Exception {
 
 		logger.info("regist get ...........");
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String registPOST(Com_boardVO board, RedirectAttributes rttr) throws Exception {
+	public String registPOST(Com_staffVO board, RedirectAttributes rttr, @RequestParam("imgFile") MultipartFile imgFile)
+			throws Exception {
 
 		logger.info("regist post ...........");
 		logger.info(board.toString());
 
+		String img_filenm = UploadFileUtils.uploadImageFile(uploadPath, imgFile.getOriginalFilename(),
+				imgFile.getBytes(), img_fileName + (service.maxNum() + 1), dirPath);
+		board.setImg_filenm(img_filenm);
 		service.regist(board);
 
 		rttr.addFlashAttribute("msg", "SUCCESS");
 
-		return "redirect:/noticeboard/list?page=1";
+		return "redirect:/sboard/list";
 	}
+
 }
